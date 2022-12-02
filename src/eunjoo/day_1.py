0e1ac01055aa8197a84173dfdac9368ff7f0e07c
Eunjoo Byeon <eunjoo@workbravely.com>
2022-12-02 15:35:40 -0500

    mine

---
+++ src/eunjoo/day_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+input_path = '../input/221201.txt'
+
+with open(input_path, 'r') as f:
+    input_ = f.read().splitlines()
+
+in_cc = ', '.join([x for x in input_])
+tmp = in_cc.split(", , ")
+
+total_cals_ls = [sum([int(x) for x in gr.split(', ')]) for gr in tmp]
+
+print("Day 1-1: The most calories", end = ": ")
+print(max(total_cals_ls))
+
+total_cals_ls.sort(reverse = True)
+print("Day 1-2: Sum of top 3 calories", end = ": ")
+print(sum(total_cals_ls[0:3]))
+
